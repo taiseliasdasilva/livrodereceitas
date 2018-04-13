8065e53ca0716a5f530a8c602cee64b91b8e9c98
Aluno <Aluno@SNPRTLAB02_11>
2018-04-12 22:16:36 -0300

    programacao atualizada

--- LivroDeReceitas/LivroDeReceitas/CadReceitas.aspx.cs
+++ LivroDeReceitas/LivroDeReceitas/CadReceitas.aspx.cs
@@ -14,7 +14,46 @@ namespace LivroDeReceitas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (IsPostBack)
+                return;
+            
         }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+                Salvar();
+            LimparCampos();
+            Response.Redirect("~/VisualizarReceitas.aspx");
+        }
+
+       
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/VisualizarReceitas.aspx");
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = string.Empty;
+            txtIngredientes.Text = string.Empty;
+            txtModo.Text = string.Empty;
+        }
+
+        private bool Validar()
+        {
+            if (string.IsNullOrWhiteSpace(txtNome.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txtIngredientes.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txtModo.Text))
+                return false;
+        }
+
+        private void Salvar()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
