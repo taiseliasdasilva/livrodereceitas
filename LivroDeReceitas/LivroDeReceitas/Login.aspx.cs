1ee198d235c93077edc9ac25a38319d83d482c26
taiseliasdasilva <taiseliasdasilva@gmail.com>
2018-05-02 22:05:00 -0300

    login alterado

--- LivroDeReceitas/LivroDeReceitas/Login.aspx.cs
+++ LivroDeReceitas/LivroDeReceitas/Login.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using LivroDeReceitas.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -21,7 +23,63 @@ namespace LivroDeReceitas
         {
             Response.Redirect("~/CadUsuario.aspx");
 
+            if (Validar())
+                Salvar();
+            LimparCampos();
+            Response.Redirect("~/CadUsuario.aspx");
+        }
 
+        private bool Validar()
+        {
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txtSenha.Text))
+                return false;
+
+            return true;
+        }
+
+
+
+        private void LimparCampos()
+        {
+            txtEmail.Text = string.Empty;
+            txtSenha.Text = string.Empty;
+        }
+
+        private void Salvar()
+        {
+            var obj = new Usuario();
+            obj.Email = txtEmail.Text;
+            obj.Senha = txtSenha.Text;
+
+
+            using (SqlConnection conn =
+                new SqlConnection(@"Initial Catalog=RECEITAS;
+                        Data Source=localhost;
+                        Integrated Security=SSPI;"))
+            {
+                string strSQL = @"INSERT INTO usuario (email, senha) 
+                                  VALUES (@email, @senha)";
+
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    cmd.Connection = conn;
+
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = obj.Email;
+                    cmd.Parameters.Add("@senha", SqlDbType.Int).Value = obj.Senha;
+
+
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    conn.Close();
+
+                }
+            }
         }
     }
 }
